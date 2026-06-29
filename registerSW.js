1cd5f93ed13e326b850c92e8415c27b9b549ec7e
SaifulSk <ssaifrocks@gmail.com>
2026-06-29 16:08:54 +0530

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tuition-plus-management/sw.js', { scope: '/tuition-plus-management/' })})}
